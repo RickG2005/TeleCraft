37a0d5b19f5c8a4bd4ef29b126938e5e7fdacb5e
Rick Gonsalves <rick2005.g@gmail.com>
2026-05-27 16:35:07 +0530

    chore: Initial commit and setup

---
+++ src/main/java/com/github/rickg2005/telecraft/TeleCraftApplication.java
@@ -0,0 +1,13 @@
+package com.github.rickg2005.telecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeleCraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/rickg2005/telecraft/TeleCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rickg2005.telecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
